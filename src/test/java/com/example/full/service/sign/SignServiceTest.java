ef59a82d1b3903de0bca39f1dd03a90a98b04be6
BYEONGRYEOL <lyulbyung@gmail.com>
2023-07-23 22:37:55 +0900

    signservicetest 작성중 membernotfoundtest 하다말았엉

--- src/test/java/com/example/full/service/sign/SignServiceTest.java
+++ src/test/java/com/example/full/service/sign/SignServiceTest.java
@@ -6,9 +6,13 @@ import com.example.full.dto.sign.SignUpRequest;
 import com.example.full.entity.member.Member;
 import com.example.full.entity.member.Role;
 import com.example.full.entity.member.RoleType;
+import com.example.full.exception.LoginFailureException;
+import com.example.full.exception.MemberEmailAlreadyExistsException;
+import com.example.full.exception.MemberNicknameAlreadyExistsException;
 import com.example.full.exception.RoleNotFoundException;
 import com.example.full.repository.member.MemberRepository;
 import com.example.full.repository.member.RoleRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,8 +24,8 @@ import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.*;
@@ -82,5 +86,50 @@ class SignServiceTest {
 
     }
 
+    @Test
+    @DisplayName("회원가입시 이메일중복 테스트")
+    void duplicateEmailSignUpTest() {
+        //given
+        SignUpRequest req = new SignUpRequest(anyString(), anyString(), anyString(), anyString());
+        given(memberRepository.existsByEmail(anyString())).willReturn(true);
+
+        //when
+        //then
+        assertThatThrownBy(() -> signService.signUp(req)).isInstanceOf(MemberEmailAlreadyExistsException.class);
+    }
+
+    @Test
+    @DisplayName("회원가입 닉네임 중복 테스트")
+    void duplicateNickNameSignUpTest() {
+        //given
+        SignUpRequest req = new SignUpRequest(anyString(), anyString(), anyString(), anyString());
+        given(memberRepository.existsByNickname(anyString())).willReturn(true);
+        //when
+        //then
+        assertThatThrownBy(()-> signService.signUp(req)).isInstanceOf(MemberNicknameAlreadyExistsException.class);
+    }
+
+    @Test
+    @DisplayName("")
+    void signUpRoleNotFoundTest() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    @DisplayName("회원정보 NotFound 테스트")
+    void signInMemberNotFoundTest() {
+        // repository가 못찾은경우 빈 Optional 객체를 반환, Optional.empty()
+        //given
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.empty());
+        //when
+        //then
+        assertThatThrownBy(()->signService.signIn(anySignInRequest())).isInstanceOf(LoginFailureException.class);
+    }
+
+    private SignInRequest anySignInRequest(){
+        return new SignInRequest(anyString(), anyString());
+    }
 
 }
